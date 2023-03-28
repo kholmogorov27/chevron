04c753b24dc18d5a966ab88f593a7d9e15428b57
Flexusma <info@flexusma.de>
2023-03-28 11:13:26 +0200

    use better url detection
    - now detects http://localhost and http://localhost:xxxx
    - works with ip addresses

--- src/functions/webUtils/isValidURL.js
+++ src/functions/webUtils/isValidURL.js
@@ -1,6 +1,6 @@
 function isValidUrl(url) {
   // eslint-disable-next-line no-useless-escape
-  const regex = new RegExp(/^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/)
+  const regex = new RegExp('^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$')
 
   if (url.match(regex)) return true
   return false
