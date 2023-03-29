aba5e5be4d833451094a9781af449dae190e7037
Flexusma <38422530+Flexusma@users.noreply.github.com>
2023-03-29 23:38:14 +0200

    use better url detection (#16)
    * use better url detection
    
    - now detects http://localhost and http://localhost:xxxx
    - works with ip addresses
    
    * accidentally used an invalid regex

--- src/functions/webUtils/isValidURL.js
+++ src/functions/webUtils/isValidURL.js
@@ -1,7 +1,12 @@
 function isValidUrl(url) {
   // eslint-disable-next-line no-useless-escape
-  const regex = new RegExp(/^[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/)
-
+  const regex = new RegExp('^([a-zA-Z]+:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR IP (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+      '(\\#[-a-z\\d_]*)?$', // fragment locator
+      'i');
   if (url.match(regex)) return true
   return false
 }
